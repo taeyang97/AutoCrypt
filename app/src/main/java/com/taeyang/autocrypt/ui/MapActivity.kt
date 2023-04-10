c3df9c740226957876b50ae8a25d448d798c6290
Taeyang <zlaxodid1@naver.com>
2023-04-10 20:45:21 +0900

    build: add Activity Ui

---
+++ app/src/main/java/com/taeyang/autocrypt/ui/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.taeyang.autocrypt.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.taeyang.autocrypt.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
---
+++ app/src/main/java/com/taeyang/autocrypt/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.taeyang.autocrypt.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.taeyang.autocrypt.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
